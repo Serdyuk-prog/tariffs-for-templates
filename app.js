ce8dd845e788dd461fed8ff2e58792ac5ec414fc
Serdyuk-prog <tony.not.gloomy@gmail.com>
2022-02-09 15:34:03 +0700

    main backend without ML part is done

--- app.js
+++ app.js
@@ -2,14 +2,23 @@ const express = require("express");
 const path = require("path");
 const upload = require("express-fileupload");
 const fs = require("fs");
+const AppError = require("./AppError");
+const morgan = require("morgan"); // consol loging tool
+const cp = require("child_process");
 
 const app = express();
 let decoder = new TextDecoder();
 const encoder = new TextEncoder();
 
+function toPython(text) {
+    const { stdout, stderr } = cp.spawnSync("python3", ["codespace/codespace.py", text]);
+    return stdout.toString();
+}
+
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
+// app.use(morgan("dev")); // will log every request info
 app.use(express.urlencoded({ extended: true }));
 app.use(upload());
 
@@ -17,28 +26,31 @@ app.get("/", (req, res) => {
     res.render("index");
 });
 
-app.post("/", (req, res) => {
+app.post("/", (req, res, next) => {
     if (req.files) {
         const file = req.files.file;
-        console.dir(file);
-        console.log(decoder.decode(file.data));
+        const text = decoder.decode(file.data);
+        const result = toPython(text);
 
-        file.mv("./uploads/" + file.name, function (err) {
+        fs.writeFile("./uploads/test.csv", result, (err) => {
             if (err) {
-                res.send(err);
-            } else {
-                res.download("./uploads/" + file.name, "result.csv", (err) => {
-                    if (err) {
-                        res.send(err);
-                    } else {
-                        fs.unlinkSync("./uploads/" + file.name);
-                    }
-                });
+                return console.log(err);
             }
+            res.download("./uploads/test.csv", "result.csv", (err) => {
+                if (err) {
+                    return console.log(err);
+                }
+                fs.unlinkSync("./uploads/test.csv");
+            });
         });
     } else {
-        throw Error();
+        throw new AppError("no file provided", 400);
     }
+});
+
+app.use((err, req, res, next) => {
+    const { status = 500, message = "Something went wrong" } = err;
+    res.status(status).send(message);
 });
 
 app.listen(3000, () => {
---
+++ codespace/codespace.py
@@ -0,0 +1,5 @@
+import sys
+
+string = str(sys.argv[1])
+
+print(string.upper())
