53d9b7d3522c5ca24a432990a463853027b76276
Serdyuk-prog <tony.not.gloomy@gmail.com>
2022-02-18 18:15:37 +0700

    done but needs testing

--- app.js
+++ app.js
@@ -39,6 +39,7 @@ app.set("views", path.join(__dirname, "views"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(upload());
+app.use(express.static(path.join(__dirname, 'views')))
 
 app.get("/", (req, res) => {
     res.render("index");
